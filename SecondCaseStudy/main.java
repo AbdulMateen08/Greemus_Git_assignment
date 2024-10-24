1640db450a2f242bf75c324633de62fe014affa2
AbdulMateen08 <mdmateen147@gmail.com>
2024-10-24 13:29:10 +0530

    commiting the changes

---
+++ SecondCaseStudy/main.java
@@ -0,0 +1,8 @@
+package SecondCaseStudy;
+
+public class main {
+    public static void main(String[] args) {
+        System.out.println("Main branch");
+        System.out.println("new featrue branch");
+    }
+}
